cc7e917c1fb8502d3c4cfa488d8f4c84c556ec35
Miguel Domínguez <migueldominguezguevara@gmail.com>
2022-05-04 12:52:11 -0500

    added functionality to menu and responsiveness

--- js/nav.js
+++ js/nav.js
@@ -3,8 +3,20 @@
 //Variables
 const mobileNav = document.getElementById('mobile-nav');
 const hamburgerBtn = document.getElementById('hamburger-btn');
+const homeBtn = document.getElementById('home-btn');
+const aboutBtn = document.getElementById('about-btn');
+const resumeBtn = document.getElementById('resume-btn');
+const portfolioBtn = document.getElementById('portfolio-btn');
+const contactBtn = document.getElementById('contact-btn');
+const navBtns = [hamburgerBtn, homeBtn, aboutBtn, resumeBtn, portfolioBtn, contactBtn];
+
+//Functions
+
+function toggleMobileMenu(){
+    mobileNav.classList.toggle('hidden');
+}
 
 //Event Listeners
-hamburgerBtn.addEventListener('click', ()=>{
-    mobileNav.classList.toggle('hidden');
+navBtns.forEach( item =>{
+    item.addEventListener('click', toggleMobileMenu);
 });
